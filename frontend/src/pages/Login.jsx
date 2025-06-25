123e8046c39ea0bb34fa4d961bb9ed8cec3d24e9
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-25 22:27:30 +0530

    implement form handling with react-hook-form in Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+
+    const {register, handleSubmit, reset} = useForm();
+
+    const submitHandler = (data) =>{
+        console.log(data)
+    }
+
   const fields = [
     { type: "text", label: "Name" },
     { type: "email", label: "Email" },
@@ -8,7 +16,7 @@ const Login = () => {
   ];
 
   return (
-    <form className="bg-zinc-900 w-[40vw] rounded-lg shadow-xl px-5 py-10">
+    <form onSubmit={handleSubmit(submitHandler)} className="bg-zinc-900 w-[40vw] rounded-lg shadow-xl px-5 py-10">
       <h1 className="text-center text-2xl">Login</h1>
       <>
         {fields.map((field, index) => (
@@ -16,23 +24,13 @@ const Login = () => {
             <label htmlFor="email">{field.label}</label>
             <input
               type={field.type}
+              {...register(`${field.label}`)}
               className="px-3 py-2 rounded-md border-2 outline-none border-zinc-700 "
             />
           </div>
         ))}
       </>
-      {/* <div className="flex flex-col gap-2">
-        <label htmlFor="name">Name</label>
-        <input type="text" className="px-3 py-2 rounded-md border-2 outline-none border-zinc-600 " />
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="email">Email</label>
-        <input type="email" className="px-3 py-2 rounded-md border-2 outline-none border-zinc-600 " />
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="password">Password</label>
-        <input type="password" className="px-3 py-2 rounded-md border-2 outline-none border-zinc-600 " />
-      </div> */}
+      <button className="px-3 mt-5 w-full py-1 font-semibold bg-green-600 rounded-md">Login</button>
     </form>
   );
 };
