907bf46de10a21505ee62b06aa520203123ef02b
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-25 11:23:13 +0530

    integrate Redux store for state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,5 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import BrowserRouter from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
