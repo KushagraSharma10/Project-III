718d5d86e1c9e39ab85d866ee2773685b1e77307
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-25 23:24:10 +0530

    user registration form and update user data handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,9 @@
 import React, { useEffect } from "react";
-import { asyncGetUsers } from "./store/actions/userActions";
 
-import { useDispatch, useSelector } from "react-redux";
 import MainRoutes from "./routes/MainRoutes";
 import Nav from "./components/Nav";
 
 const App = () => {
-  const data = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  console.log(data);
-
-  useEffect(() => {
-    dispatch(asyncGetUsers());
-  }, []);
 
   return (
     <div className="w-screen px-[10%] h-screen bg-zinc-800 text-white">
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,42 +1,58 @@
-import React from 'react'
-import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { asyncRegisterUser } from "../store/actions/userActions";
+import { nanoid } from "@reduxjs/toolkit";
 
 const Register = () => {
+  const { register, handleSubmit, reset } = useForm();
 
-  const {register, handleSubmit, reset} = useForm();
+  const dispatch = useDispatch();
 
-  const submitHandler = (data) =>{
-      console.log(data)
-  }
+  const RegisterHandler = (user) => {
+    user.id = nanoid();
+    user.isAdmin = false;
+    console.log(user);
+    dispatch(asyncRegisterUser(user));
+  };
 
-const fields = [
-  { type: "text", label: "Name" },
-  { type: "email", label: "Email" },
-  { type: "password", label: "Password" },
-];
+  const fields = [
+    { type: "text", label: "Name", placeholder :"John Doe"},
+    { type: "email", label: "Email" ,placeholder :"JohnDoe@gmail.com" },
+    { type: "password", label: "Password", placeholder :"******" },
+  ];
 
   return (
-    <form onSubmit={handleSubmit(submitHandler)} className="bg-zinc-900 w-[40vw] rounded-lg shadow-xl px-5 py-10">
-    <h1 className="text-center text-2xl">Register</h1>
-    <>
-      {fields.map((field, index) => (
-        <div key={index} className="flex flex-col mt-3 gap-2">
-          <label htmlFor="email">{field.label}</label>
-          <input
-            type={field.type}
-            {...register(`${field.label}`)}
-            className="px-3 py-2 rounded-md border-2 outline-none border-zinc-700 "
-          />
-        </div>
-      ))}
-    </>
-    <button className="px-3 mt-5 w-full py-1 font-semibold bg-green-600 rounded-md">Register User</button>
-    <p className="mt-5">
-      Already have an account? <Link to = "/login" className = "text-blue-400">Login</Link>
-    </p>
-  </form>
-  )
-}
+    <form
+      onSubmit={handleSubmit(RegisterHandler)}
+      className="bg-zinc-900 w-[40vw] rounded-lg shadow-xl px-5 py-10"
+    >
+      <h1 className="text-center text-2xl">Register</h1>
+      <>
+        {fields.map((field, index) => (
+          <div key={index} className="flex flex-col mt-3 gap-2">
+            <label htmlFor="email">{field.label}</label>
+            <input
+              type={field.type}
+              {...register(`${field.label}`)}
+              className="px-3 py-2 rounded-md border-2 outline-none border-zinc-700 "
+              placeholder={field.placeholder}
+            />
+          </div>
+        ))}
+      </>
+      <button className="px-3 mt-5 w-full py-1 font-semibold bg-green-600 rounded-md">
+        Register User
+      </button>
+      <p className="mt-5">
+        Already have an account?{" "}
+        <Link to="/login" className="text-blue-400">
+          Login
+        </Link>
+      </p>
+    </form>
+  );
+};
 
-export default Register
+export default Register;
