f92eb8152833e6d82948e2096b62d49b2e4f3c67
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-25 22:13:48 +0530

    enhance Login page with form fields for user input

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className='flex items-center py-10 justify-center mb-10 space-x-10'>
-        <NavLink to={"/"}>Home</NavLink>
-        <NavLink to ="/products">Products</NavLink>
-        <NavLink to = "/login">Login</NavLink>
+    <nav className="flex items-center py-10 justify-center mb-10 space-x-10">
+      <NavLink to={"/"}>Home</NavLink>
+      <NavLink to="/products">Products</NavLink>
+      <NavLink to="/login">Login</NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+  return (
+    <form className="bg-zinc-900 w-[50vw] px-5 py-10">
+      <h1 className="text-center text-2xl">Login</h1>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="name">Name</label>
+        <input type="text" className="px-3 py-2 rounded-md border-2 outline-none border-zinc-600 " />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email">Email</label>
+        <input type="email" className="px-3 py-2 rounded-md border-2 outline-none border-zinc-600 " />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="password">Password</label>
+        <input type="password" className="px-3 py-2 rounded-md border-2 outline-none border-zinc-600 " />
+      </div>
+    </form>
+  );
+};
+
+export default Login;
