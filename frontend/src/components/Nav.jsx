735a1c3b55a7690da4090035da4f88c2e28e3d73
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-25 22:05:04 +0530

    add navigation links and implement Login, Products, and Register pages

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,8 +4,9 @@ import { NavLink } from 'react-router-dom'
 const Nav = () => {
   return (
     <nav className='flex items-center py-10 justify-center mb-10 space-x-10'>
-        <NavLink>Home</NavLink>
-        <NavLink>Products</NavLink>
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to ="/products">Products</NavLink>
+        <NavLink to = "/login">Login</NavLink>
     </nav>
   )
 }
--- frontend/src/routes/MainRoutes.jsx
+++ frontend/src/routes/MainRoutes.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from '../pages/Home';
+import Products from '../pages/Products';
+import Login from '../pages/Login';
 
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path = "/" element = {<Home />} />
+        <Route path = "/products" element = {<Products />} />
+        <Route path = "/login" element = {<Login />} />
     </Routes>
 )
 }
