a7247276e4629a78c23b60d500ee8ce9bb4deea0
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-26 08:14:21 +0530

    add async user loading and improve login action handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Nav from "./components/Nav";
 
 const App = () => {
 
+    
+
   return (
     <div className="w-screen px-[10%] h-screen bg-zinc-800 text-white">
       <Nav />
--- frontend/src/store/actions/userActions.jsx
+++ frontend/src/store/actions/userActions.jsx
@@ -1,29 +1,41 @@
 import axios from "../../api/axiosconfig";
+import { loadUser } from "../reducers/userSlice";
 
+export const asyncCurrentUser = () => async (dispatch, getState) => {
+  try {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if(user) dispatch(loadUser(user))
+    else console.log("User not logged in!")  
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const asyncLoginUser = (user) => async (dispatch, getState) => {
   try {
-    const {data} = await axios.get(`/users?Email=${user.email}&Password=${user.password}`)
+    const { data } = await axios.get(
+      `/users?Email=${user.Email}&Password=${user.Password}`
+    );
     console.log(data[0]);
-    localStorage.setItem("user", JSON.stringify(data[0]))    
+    localStorage.setItem("user", JSON.stringify(data[0]));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 export const asyncLogoutUser = (user) => async (dispatch, getState) => {
   try {
-    localStorage.setItem("user", null)    
+    localStorage.setItem("user", null);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 export const asyncRegisterUser = (user) => async (dispatch, getState) => {
   try {
-    const res = await axios.post("/users", user)
-    console.log(res)
+    const res = await axios.post("/users", user);
+    console.log(res);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
