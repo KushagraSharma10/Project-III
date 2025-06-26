bb58dc54d7a820d120a1d3adef60ffcc37eb3a4d
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-26 08:51:09 +0530

    add admin routes for creating and updating products

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 const Nav = () => {
+  
+  const user = useSelector((state) => state.userReducer.users)
+  console.log(user)
+
   return (
     <nav className="flex items-center py-10 justify-center mb-10 space-x-10">
       <NavLink to={"/"}>Home</NavLink>
       <NavLink to="/products">Products</NavLink>
       <NavLink to="/login">Login</NavLink>
+      <NavLink to="/admin/create-product">Create Product</NavLink>
     </nav>
   );
 };
