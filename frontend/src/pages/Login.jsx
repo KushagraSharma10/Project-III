d52b6025bc617053b278c4caaffa781e722f0eb6
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-26 07:57:38 +0530

    implement login functionality with async user authentication

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { asyncLoginUser } from "../store/actions/userActions";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
+  const { register, handleSubmit, reset } = useForm();
 
-    const {register, handleSubmit, reset} = useForm();
-
-    const submitHandler = (data) =>{
-        console.log(data)
-    }
+  const dispatch = useDispatch();
+  
+  const LoginHandler = (user) => {
+    dispatch(asyncLoginUser(user));
+    console.log(user);
+  };
 
   const fields = [
     { type: "email", label: "Email" },
@@ -16,7 +20,10 @@ const Login = () => {
   ];
 
   return (
-    <form onSubmit={handleSubmit(submitHandler)} className="bg-zinc-900 w-[40vw] rounded-lg shadow-xl px-5 py-10">
+    <form
+      onSubmit={handleSubmit(LoginHandler)}
+      className="bg-zinc-900 w-[40vw] rounded-lg shadow-xl px-5 py-10"
+    >
       <h1 className="text-center text-2xl">Login</h1>
       <>
         {fields.map((field, index) => (
@@ -30,9 +37,14 @@ const Login = () => {
           </div>
         ))}
       </>
-      <button className="px-3 mt-5 w-full py-1 font-semibold bg-green-600 rounded-md">Login User</button>
+      <button className="px-3 mt-5 w-full py-1 font-semibold bg-green-600 rounded-md">
+        Login User
+      </button>
       <p className="mt-5">
-        Don't have an account? <Link to = "/register" className = "text-blue-400">Register</Link>
+        Don't have an account?{" "}
+        <Link to="/register" className="text-blue-400">
+          Register
+        </Link>
       </p>
     </form>
   );
--- frontend/src/store/actions/userActions.jsx
+++ frontend/src/store/actions/userActions.jsx
@@ -1,6 +1,16 @@
 import axios from "../../api/axiosconfig";
 
-export const asyncRegisterUser = (user) => async (dispatchEvent, getState) => {
+
+export const asyncLoginUser = (user) => async (dispatch, getState) => {
+  try {
+    const res = await axios.get(`/users?Email=${user.email}&Password=${user.password}`)
+    console.log(res);    
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const asyncRegisterUser = (user) => async (dispatch, getState) => {
   try {
     const res = await axios.post("/users", user)
     console.log(res)
