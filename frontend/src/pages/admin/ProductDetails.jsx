2ded56939e09e797970caed8711e3eb2164555f9
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-27 08:07:11 +0530

    add Cart page and enhance ProductDetails layout with image and details

--- frontend/src/pages/admin/ProductDetails.jsx
+++ frontend/src/pages/admin/ProductDetails.jsx
@@ -3,12 +3,32 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-  const {id} = useParams();
+  const { id } = useParams();
   const products = useSelector((state) => state.productReducer.products);
-  const product = products?.find((product)=> product.id === id)
-  console.log(product) 
+  const product = products?.find((product) => product.id === id);
+  console.log(product);
 
-  return <div>ProductDetails</div>;
+  return (
+    <div className="w-full">
+      <div className="flex">
+        <div className="w-[50vw]">
+          <div className="w-[30vw]">
+            <img
+              src={product.image}
+              alt=""
+              className="w-full h-[50vh] object-cover"
+            />
+          </div>
+        </div>
+        <div className="px-5 h-full">
+          <h1 className="text-3xl">{product.title}</h1>
+          <p className="text-2xl mt-3 text-green-400">${product.price}</p>
+          <p className="mt-5">{product.description}</p>
+        </div>
+      </div>
+      
+    </div>
+  );
 };
 
 export default ProductDetails;
--- frontend/src/store/actions/productActions.jsx
+++ frontend/src/store/actions/productActions.jsx
@@ -1,20 +1,19 @@
 import axios from "../../api/axiosconfig";
 import { loadProduct } from "../reducers/productSlice";
 
-export const asyncLoadProducts = () => async(dispatch, getState) =>{
-    try {
-        const {data} = await axios.get("/products")
-        dispatch(loadProduct(data))
-    } catch (error) {
-        console.log(error)
-    }
-}
+export const asyncLoadProducts = () => async (dispatch, getState) => {
+  try {
+    const { data } = await axios.get("/products");
+    dispatch(loadProduct(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const asyncCreateProduct = (product) => async (dispatch, getState) => {
   try {
-  
-    await axios.post("/products", product)
-    dispatch(asyncLoadProducts())
+    await axios.post("/products", product);
+    dispatch(asyncLoadProducts());
   } catch (error) {
     console.log(error);
   }
