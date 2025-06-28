45daf55d68bfb37a8ff9bcb93479ef1157f18846
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-06-28 09:53:22 +0530

    implement cart item display in Cart component

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -6,8 +6,27 @@ const Cart = () => {
   const users = useSelector((state) => state.userReducer.users);
   const products = useSelector((state) => state.productReducer.products);
 
+  const cartItems = users.cart.map((c)=> (
+    <li key={c.product.id} className='flex items-center justify-between'>
+      <img 
+      className='w-[10vmax] h-[10vmax] object-cover'
+      src = {c.product.image} 
+      alt= ""
+      />
+
+      <span>{c.product.title}</span>
+      <span>$ {c.product.price}</span>
+      <p>
+        <span className='text-xl'>-</span>
+        <span className='mx-3 p-2 rounded bg-gray-700'>{c.quantity}</span>
+        <span className='text-lg'>+</span>
+      </p>
+    </li>
+  ))
+
+
   return (
-    <div>Cart</div>
+    <div>{cartItems}</div>
   )
 }
 
